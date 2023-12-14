2d94e9eb2a1dc42b9ac29f7c674bd621940789b0
Naikogod <131040211+Naikogod@users.noreply.github.com>
2023-12-14 02:34:30 -0300

    ThingsSpeak

--- app/src/main/java/com/example/proyectomorse/MainActivity.java
+++ app/src/main/java/com/example/proyectomorse/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.proyectomorse;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.proyectomorse.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editTextMessage;
     private Button buttonSend;
-    private FirebaseFirestore db;
+    private String apiKey = "UDN65FSGH7Y06YL8"; // Reemplaza con tu API Key de ThingSpeak
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +28,6 @@ public class MainActivity extends AppCompatActivity {
         editTextMessage = findViewById(R.id.editTextMessage);
         buttonSend = findViewById(R.id.buttonSend);
 
-        // Inicializar Firestore
-        db = FirebaseFirestore.getInstance();
-
-        // Configurar el evento de clic del botón
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,56 +37,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void enviarMensaje() {
-        // Obtener el mensaje del EditText y eliminar espacios al inicio y al final
         String mensaje = editTextMessage.getText().toString().trim().toUpperCase();
-
-        // 1. Validar que el mensaje no esté vacío
         if (mensaje.isEmpty()) {
-            // Mostrar mensaje de error si el mensaje está vacío
             Toast.makeText(MainActivity.this, "Por favor, ingresa un mensaje antes de enviarlo", Toast.LENGTH_LONG).show();
-            return; // Salir del método porque el mensaje está vacío
+            return;
         }
 
-        // 2. Validar que el mensaje no tenga espacios al inicio ni al final
-        if (mensaje.startsWith(" ") || mensaje.endsWith(" ")) {
-            // Mostrar mensaje de error si el mensaje tiene espacios al inicio o al final
-            Toast.makeText(MainActivity.this, "Evita espacios al inicio o al final del mensaje", Toast.LENGTH_LONG).show();
-            return; // Salir del método porque el mensaje tiene espacios al inicio o al final
+        // Aquí llamamos a la tarea asincrónica para enviar datos a ThingSpeak
+        new SendToThingSpeakTask().execute(mensaje);
+    }
+
+    private class SendToThingSpeakTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                String mensaje = params[0];
+                URL url = new URL("https://api.thingspeak.com/update.json");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                OutputStream os = urlConnection.getOutputStream();
+                os.write(("api_key=" + apiKey + "&field1=" + mensaje).getBytes());
+                os.flush();
+                os.close();
+
+                int responseCode = urlConnection.getResponseCode();
+                return responseCode == HttpURLConnection.HTTP_OK;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
         }
 
-        // 3. Validar que el mensaje solo contenga letras del alfabeto y espacios
-        if (!mensaje.matches("[A-Z ]+")) {
-            // Mostrar mensaje de error si el mensaje no cumple con los requisitos
-            Toast.makeText(MainActivity.this, "Por favor, ingresa un mensaje válido (letras del alfabeto A-Z y espacios)", Toast.LENGTH_LONG).show();
-            return; // Salir del método porque el mensaje no cumple con los requisitos
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                editTextMessage.setText("");
+                Toast.makeText(MainActivity.this, "Mensaje enviado con éxito a ThingSpeak", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Hubo un problema al enviar el mensaje", Toast.LENGTH_SHORT).show();
+            }
         }
-
-        // 4. Validar que el mensaje no contenga más de un espacio consecutivo entre letras
-        if (mensaje.contains("  ")) {
-            // Mostrar mensaje de error si el mensaje tiene más de un espacio consecutivo
-            Toast.makeText(MainActivity.this, "Evita espacios consecutivos entre letras", Toast.LENGTH_LONG).show();
-            return; // Salir del método porque el mensaje tiene más de un espacio consecutivo
-        }
-
-        // 5. Enviar el mensaje a Firestore
-        Map<String, Object> data = new HashMap<>();
-        data.put("mensaje", mensaje);
-
-        db.collection("mensajes")
-                .add(data)
-                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
-                        if (task.isSuccessful()) {
-                            // Limpiar el campo de texto después de enviar el mensaje
-                            editTextMessage.setText("");
-                            // 6. Mostrar mensaje de éxito
-                            Toast.makeText(MainActivity.this, "Mensaje enviado con éxito", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // 7. Manejar errores aquí
-                            Toast.makeText(MainActivity.this, "Hubo un problema al enviar el mensaje", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 }
+
